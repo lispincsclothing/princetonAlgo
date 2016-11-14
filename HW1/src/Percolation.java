3596a53eddb64aa6ddfb28d6eaf2107cdd7ee84d
Name <email>
2016-11-13 22:33:22 -0800

    WIP - Submitted week 1

--- HW1/src/Percolation.java
+++ HW1/src/Percolation.java
@@ -4,6 +4,8 @@
 
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import edu.princeton.cs.algs4.StdOut;
 
 public class Percolation {
     public static void main(String[] args) {
---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,9 @@
+/**
+ * Created by student on 11/4/16.
+ */
+public class Hello {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}

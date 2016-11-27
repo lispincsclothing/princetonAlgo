34594bf5c53303fe27bf1b462d5c160edb5ca914
Student <student@develops-MacBook-Pro.local>
2016-11-27 09:42:01 -0800

    Reoranized so that lib location of global libraries

---
+++ WEEK1/QuickFindUF/src/QuickFindUF.java
@@ -0,0 +1,20 @@
+/**
+ * Created by student on 11/26/16.
+ */
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class QuickFindUF {
+    public static void main(String[] args) {
+        int n = StdIn.readInt();
+        QuickFindUF uf = new QuickFindUF(n);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.connected(p, q)) continue;
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
+        StdOut.println(uf.count() + " components");
+    }
+}

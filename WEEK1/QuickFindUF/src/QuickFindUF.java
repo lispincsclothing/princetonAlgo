ad0799d2616cc5db29a0f0231dc80cc9a3028efd
Student <student@develops-MacBook-Pro.local>
2016-12-03 18:49:22 -0800

    WIP before submission HW 1

--- WEEK1/QuickFindUF/src/QuickFindUF.java
+++ WEEK1/QuickFindUF/src/QuickFindUF.java
@@ -5,6 +5,9 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class QuickFindUF {
+    private int[] id;    // id[i] = component identifier of i
+    private int count;   // number of components
+
     public static void main(String[] args) {
         int n = StdIn.readInt();
         QuickFindUF uf = new QuickFindUF(n);
@@ -16,5 +19,43 @@ public class QuickFindUF {
             StdOut.println(p + " " + q);
         }
         StdOut.println(uf.count() + " components");
+    }
+
+    public QuickFindUF(int n) {
+        count = n;
+        id = new int[n];
+        for (int i = 0; i < n; i++)
+            id[i] = i;
+    }
+
+    public void union(int p, int q) {
+        validate(p);
+        validate(q);
+        int pID = id[p];   // needed for correctness
+        int qID = id[q];   // to reduce the number of array accesses
+
+        // p and q are already in the same component
+        if (pID == qID) return;
+
+        for (int i = 0; i < id.length; i++)
+            if (id[i] == pID) id[i] = qID;
+        count--;
+    }
+
+    public boolean connected(int p, int q) {
+        validate(p);
+        validate(q);
+        return id[p] == id[q];
+    }
+
+    private void validate(int p) {
+        int n = id.length;
+        if (p < 0 || p >= n) {
+            throw new IndexOutOfBoundsException("index " + p + " is not between 0 and " + (n-1));
+        }
+    }
+
+    public int count() {
+        return count;
     }
 }

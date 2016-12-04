d4735e7d60c7b2224baa18d0fc074928c9b60a41
Student <student@develops-MacBook-Pro.local>
2016-12-04 14:30:41 -0800

    Working Percolation and PercolationStats  - haven't tested validity

--- HW/HW1/src/Percolation.java
+++ HW/HW1/src/Percolation.java
@@ -8,41 +8,116 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import edu.princeton.cs.algs4.StdOut;
 
 public class Percolation {
-    private int gridSizeInt;
-    private int[] sites;
+    private WeightedQuickUnionUF union;
+    private WeightedQuickUnionUF backwash;
+    private boolean[] openSites;
+    private final int SIZE, TOP_INDEX, BOTTOM_INDEX;
+
 
     public Percolation(int rowSizeInt)                // create n-by-n grid, with all sites blocked
     {
-        if (rowSizeInt <= 0){
+        if (rowSizeInt <= 0) {
             throw new IllegalArgumentException("N must be greater than 0.");
         }
 
-        gridSizeInt = (rowSizeInt^2) + 2; // grid stored in single array, squared, plus the top and bottom virtual sites
+        // grid stored in single array, squared, plus the top and bottom virtual sites
+        int numNodes = rowSizeInt * rowSizeInt + 2;
 
-        //Standary declaration and initialization - same as QuickUnionUF
-        sites = new int[gridSizeInt];
-        for (int i = 0; i < gridSizeInt ; i++) {
-            sites[i]=i;
+        union = new WeightedQuickUnionUF(numNodes);
+        backwash = new WeightedQuickUnionUF(numNodes);
+        openSites = new boolean[numNodes];
+        SIZE = rowSizeInt;
+        //Q: Why are TOP and BOTTOM index assigned thusly?
+        TOP_INDEX = SIZE * SIZE;
+        BOTTOM_INDEX = SIZE * SIZE + 1;
+    }
+
+    public void open(int row, int column)       // open site (row, col) if it is not open already
+    {
+        assertSiteInRange(row, column);
+        openSites[toIndex(row, column)] = true;
+        connectToVirtualTopNode(row, column);
+        connecToAdjacentNodes(row, column);
+        connectToVirtualBottomNode(row, column);
+    }
+
+    private void assertSiteInRange(int row, int column){
+        if (row < 1 || row > SIZE || column < 1 || column > SIZE) {
+            throw new IndexOutOfBoundsException("Row or column is out of range");
         }
     }
 
+    private int toIndex(int row, int column){
+        //-1 since index starts at 0, * SIZE since going down rows, column since column offset
+        return (row - 1) * SIZE + (column - 1);
+    }
 
-    public void open(int row, int col)       // open site (row, col) if it is not open already
-    {
+    private void connectToVirtualTopNode(int row, int column){
+        if (row == 1){
+            union(TOP_INDEX, toIndex(row, column));
+        }
+    }
 
+    private void union(int p, int q){
+        union.union(p, q);
+        backwash.union(p, q);
     }
-    public boolean isOpen(int row, int col)  // is site (row, col) open?
+
+    private void connecToAdjacentNodes(int row, int column){
+        connectTopNode(row, column);
+        connectBottomNode(row, column);
+        connectLeftNode(row, column);
+        connectRightNode(row, column);
+    }
+
+    private void connectTopNode(int row, int column){
+        if(row > 1 && isOpen(row - 1, column)){
+            union(toIndex(row-1, column), toIndex(row, column));
+        }
+    }
+
+    private void connectBottomNode(int row, int column){
+        if(row < SIZE && isOpen(row + 1, column)){
+            union(toIndex(row + 1, column), toIndex(row, column));
+        }
+    }
+
+    private void connectLeftNode(int row, int column){
+        if(column > 1  && isOpen(row, column - 1)){
+            union(toIndex(row, column - 1), toIndex(row, column));
+        }
+    }
+
+    private void connectRightNode(int row, int column){
+        if(column < SIZE && isOpen(row, column + 1)){
+            union(toIndex(row, column + 1), toIndex(row, column));
+        }
+    }
+
+    private void connectToVirtualBottomNode(int row, int column) {
+        if (row == SIZE)
+        {
+            backwash.union(BOTTOM_INDEX, toIndex(row, column));
+        }
+    }
+
+    public boolean isOpen(int row, int column)  // is site (row, col) open?
     {
-        return true;
+        assertSiteInRange(row, column);
+        return openSites[toIndex(row, column)];
     }
-    public boolean isFull(int row, int col)  // is site (row, col) full?
+
+    public boolean isFull(int row, int column)  // is site (row, col) full?
     {
-        return true;
+        assertSiteInRange(row, column);
+        return union.connected(toIndex(row, column), TOP_INDEX);
     }
+
     public boolean percolates()              // does the system percolate?
     {
-        return true;
+        return backwash.connected(BOTTOM_INDEX, TOP_INDEX);
     }
+
     public static void main(String[] args)   // test client (optional)
     {
 
--- HW/HW1/src/PercolationStats.java
+++ HW/HW1/src/PercolationStats.java
@@ -1,30 +1,81 @@
+import edu.princeton.cs.algs4.Interval1D;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 /**
  * Created by student on 11/6/16.
  */
 public class PercolationStats {
+    private double results[];
+
     public PercolationStats(int n, int trials)    // perform trials independent experiments on an n-by-n grid
     {
-
+        assertPositive(n);
+        assertPositive(trials);
+        results = new double[trials];
+        runExperiments(n, trials);
     }
+
+    void assertPositive(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("Number has to be larger than 1");
+        }
+    }
+
+    private void runExperiments(int size, int numExperiments) {
+        for (int i = 0; i < numExperiments; i++) {
+            results[i] = runExperiment(size);
+        }
+    }
+
+    private double runExperiment(int size){
+        Percolation p = new Percolation(size);
+        int openSpaces = 0;
+        do {
+            int row = random(size);
+            int column = random(size);
+            if (!p.isOpen(row, column)){
+                p.open(row, column);
+                openSpaces++;
+            }
+        } while (!p.percolates());
+        return (double) openSpaces / ((double) size * size);
+    }
+
+    private int random(int size){
+        //Q: Why size + 1
+        return StdRandom.uniform(1, size+1);
+    }
+
     public double mean()                          // sample mean of percolation threshold
     {
-        return 1.0;
+        return StdStats.mean(results);
     }
+
     public double stddev()                        // sample standard deviation of percolation threshold
     {
-        return 1.0;
+        return StdStats.stddev(results);
     }
+
     public double confidenceLo()                  // low  endpoint of 95% confidence interval
     {
-        return 1.0;
+        return mean() - confidence();
+    }
+
+    private double confidence(){
+        // TODO: 12/4/16 Why 1.96
+        return(1.96 * stddev() / Math.sqrt(results.length));
     }
     public double confidenceHi()                  // high endpoint of 95% confidence interval
     {
-        return 1.0;
+        return mean() + confidence();
     }
 
     public static void main(String[] args)    // test client (described below)
     {
-
+        PercolationStats stats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        System.out.println("mean                     =" + stats.mean());
+        System.out.println("stddev                   =" + stats.stddev());
+        System.out.println("95% confidence interval  =" + stats.confidenceLo() + ", " + stats.confidenceHi());
     }
 }
